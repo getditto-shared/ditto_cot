beb0e97a86c278e47f74cbc096ab1ef1b697c98d
Kit Plummer <kit@ditto.live>
2025-07-02 17:25:39 -0400

    Kp/release prep (#7)
    * add: DittoDocument trait docs
    * update: README, and print -> log
    * update: Cargo.toml and add MIT license
    * cotditto->ditto_cot

--- fuzz/fuzz_targets/fuzz_parse_cot.rs
+++ fuzz/fuzz_targets/fuzz_parse_cot.rs
@@ -1,6 +1,6 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use cotditto::xml_parser::parse_cot;
+use ditto_cot::xml_parser::parse_cot;
 
 fuzz_target!(|data: &str| {
     let _ = parse_cot(data);
